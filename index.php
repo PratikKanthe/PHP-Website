e8048591c801955e24854f55599a408a6b1e8214
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-08-07 22:18:27 +0530

    ui change

--- index.php
+++ index.php
@@ -52,7 +52,10 @@
     <img src="bg.jpg" class="bg" alt="background" ismap />
     <div class="box">
     <div class="container">
-        <h1>Welcome To Travel Form</h1>
+
+      <!-- form -->
+      <form action="index.php" method="post">
+      <h1>Welcome To Travel Form</h1>
       <p>
         Enter your details and submit this form to confirm your participation in
         the trip
@@ -67,9 +70,6 @@
           </p>";
         }
       ?>
-
-      <!-- form -->
-      <form action="index.php" method="post">
         <input
           type="text"
           name="name"
