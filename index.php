4bea138deb5eb696009ccafdbbb989e9e136cc01
PratikKanthe <115777270+PratikKanthe@users.noreply.github.com>
2023-08-07 21:06:09 +0530

    added sql query

--- index.php
+++ index.php
@@ -7,5 +7,16 @@
     if (!$db) {
         die("Database connection failed: ". mysqli_connect_error());
     }
-    echo "Connected to database";
+    echo 'Connected to database <a href="http://localhost/firstphp/PHP-Website/index.html"> go home </a>';
+
+
+    // sql query
+    $name = ['name'];
+    $age = ['age'];
+    $gender = ['gender'];
+    $email = ['email'];
+    $number = ['number'];
+    $other = ['other'];
+    $sql = "INSERT INTO `travel` (`name`, `age`, `gender`, `email`, `number`, `other`, `dt`) VALUES ('1', 'name', '18', 'Male', 'abc@gmail.com', '9876543210', 'this is feedback', '2023-08-07 16:52:37.000000');";
+    echo $sql;
 ?>
